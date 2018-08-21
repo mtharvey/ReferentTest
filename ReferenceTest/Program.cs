ece0cb1e5b6d7452562692fdf2ab2830de2fcb6a
Mike Harvey <mharvey@async7.com>
2018-08-21 14:07:07 -0500

    Adding solution files

---
+++ ReferenceTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestLib.TestService service = new TestLib.TestService();
+            Console.WriteLine(service.GetTestMessage());
+            Console.WriteLine("Hit ENTER");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestLib/TestService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    public class TestService
+    {
+        public string GetTestMessage() { return "This is in the library..."; }
+    }
+}
